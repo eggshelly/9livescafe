f9b5253ee4b74e41079f90f8574f49aedc977798
michacg <michacg2@uci.edu>
2019-04-21 18:07:45 -0700

    Started playercontroller

---
+++ 9LivesCafe/Assets/Script/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	RigidBody m_RigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_RigidBody = GetComponent<Rigidbody>;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+
+    }
+
+}
