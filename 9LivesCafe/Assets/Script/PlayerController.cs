da85b3b1a2de7401107f0a6a9762a7d4b9f1d553
Michelle Wang <zhiyimichelle@gmail.com>
2019-06-16 22:51:28 -0700

    added some meta files/opened unity

--- 9LivesCafe/Assets/Script/PlayerController.cs
+++ 9LivesCafe/Assets/Script/PlayerController.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-	RigidBody m_RigidBody;
+	//RigidBody m_RigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
-        m_RigidBody = GetComponent<Rigidbody>;
+        //m_RigidBody = GetComponent<Rigidbody>;
     }
 
     // Update is called once per frame
