d7cc6a8e31a07d36152f4ae0cad152e201ee8e29
Michaela Gonzales <michacg2@uci.edu>
2019-07-17 20:52:54 -0700

    starting game manager

---
+++ 9LivesCafe/Assets/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] customers;
+
+    int currentCustomers = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddCustomer()
+    {
+
+    }
+}
