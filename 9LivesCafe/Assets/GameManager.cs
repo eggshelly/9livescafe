c80507516c807b481e7e3b9afefc0bfbb1c34123
Michaela Gonzales <michacg2@uci.edu>
2019-07-17 23:03:46 -0700

    starting Finite State Machine

--- 9LivesCafe/Assets/GameManager.cs
+++ 9LivesCafe/Assets/GameManager.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] customers;
+    public int custLimit = 5;
 
-    int currentCustomers = 0;
+    int custCount = 0;
 
     void Start()
     {
-        
+        InvokeRepeating("AddCustomer", 3.0f, 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void AddCustomer()
     {
-
+        if (custCount < custLimit)
+        {
+            Instantiate(customers[0], new Vector3(-5.5f, 0, custCount), transform.rotation * Quaternion.Euler(0f, 180f, 0f));
+            custCount++;
+        }
     }
 }
---
+++ 9LivesCafe/Assets/npcController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcController : MonoBehaviour
+{
+    public enum ProcessState
+    {
+        Inactive,
+
+
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
