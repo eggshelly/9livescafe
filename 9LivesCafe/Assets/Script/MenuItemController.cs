5658a5ecfd4246ea005d43b49b572e5d5149315d
Michelle Wang <zhiyimichelle@gmail.com>
2019-07-19 19:23:55 -0700

    organized files/folders
    TODO: rename npc scripts for cafe or enemy

---
+++ 9LivesCafe/Assets/Script/MenuItemController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuItemController : MonoBehaviour
+{
+
+    [SerializeField] private Vector3 PickPosition;
+    [SerializeField] private Vector3 PickRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
