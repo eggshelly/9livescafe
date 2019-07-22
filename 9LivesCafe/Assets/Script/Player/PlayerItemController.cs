197d7a0ccb4d362d47924a3bf64475d73181da07
Michelle Wang <zhiyimichelle@gmail.com>
2019-07-21 21:05:57 -0700

    WIP changes in collider/scene

--- 9LivesCafe/Assets/Script/Player/PlayerItemController.cs
+++ 9LivesCafe/Assets/Script/Player/PlayerItemController.cs
@@ -6,6 +6,7 @@ public class PlayerItemController : MonoBehaviour
 {
     private const string holdingHand = "Hip/Spine/Spine1/transform2/R_Shoulder/R_arm/R_fore_arm/R_hand";
     private GameObject hand;
+    private GameObject currHolding;
     private PlayerController playerController;
 
     private void Awake()
@@ -37,15 +38,25 @@ public class PlayerItemController : MonoBehaviour
 
     public void getItem(GameObject item)
     {
-        GameObject newitem = Instantiate(item, hand.transform.position, hand.transform.rotation);
-        MenuItemController itemController = newitem.GetComponent<MenuItemController>();
-        //makes the item a child of the hand
-        newitem.transform.SetParent(hand.transform);
-        //sets the local position to the saved center piviot point
-        newitem.transform.localPosition = itemController.getAttachPosition();
-        newitem.transform.localEulerAngles = itemController.getAttachRotaion();
-        //switch mc animation to holding
-        playerController.switchHoldingItem();
+        if (!currHolding) //can only hold something if not holding anything
+        {
+            GameObject newitem = Instantiate(item, hand.transform.position, hand.transform.rotation);       //TODO: if the item is a order don't spawn anything (or maybe add seprate animation)
+            MenuItemController itemController = newitem.GetComponent<MenuItemController>();
+            //makes the item a child of the hand
+            newitem.transform.SetParent(hand.transform);
+            //sets the local position to the saved center piviot point
+            newitem.transform.localPosition = itemController.getAttachPosition();
+            newitem.transform.localEulerAngles = itemController.getAttachRotaion();
+            //switch mc animation to holding
+            playerController.switchHoldingItem();
+
+            currHolding = item;
+        }
+        else
+        {
+            Debug.Log("TODO UPLOAD ERROR IN CANVAS THAT YOU CANNOT CARRY MORE THAN ONE ITEM");
+        }
+        
     }
 
     //TODO: make a verson which places the item on the tabel, and a version that dumps the item (e.g just gets rid of it)
@@ -59,8 +70,14 @@ public class PlayerItemController : MonoBehaviour
 
     public void placeItem()
     {
-        //switch mc animation to holding
-        Instantiate(dropItem(), Vector3.zero, Quaternion.identity);
-        playerController.switchHoldingItem();
+        if (currHolding)    //if currently holding item, can throw it away.
+        {
+            //switch mc animation to holding
+            //Instantiate(dropItem(), Vector3.zero, Quaternion.identity);   //the dropped item can still be spawned
+            playerController.switchHoldingItem();
+
+            currHolding = null;
+        }
+       
     }
 }
---
+++ 9LivesCafe/Assets/TableManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableManager : MonoBehaviour
+{
+
+    [SerializeField] private int numOrderPlaceable;
+    [SerializeField] private Vector3[] placeableLocation;
+
+    private void Awake()
+    {
+        placeableLocation = new Vector3[numOrderPlaceable];
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
